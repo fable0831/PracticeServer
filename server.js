f3b912ce8ca66d375334d911529df0bfea34d202
fable0831 <fable0831@gmail.com>
2023-01-28 23:57:34 +0900

    node와 mongoose를 활용한 임시 서버

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
-const express = require('express');
+require('dotenv').config();
+import express, { urlencoded, json } from 'express';
 const app = express();
+const router = express.Router();
+
+const { PORT } = process.env;
+
+const api = require('./api');
+
+router.use('/api', api.routes());
+
+app.use(urlencoded({ extended: true }));
+
+app.use(json());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
+});
